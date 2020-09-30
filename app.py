d3a17f8ce0f4130dab14a6aa3c759a67e7c11328
CallumCoy <gamecc@ymail.com>
2020-09-30 16:11:38 -0400

    fixed incompatable code

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ from flask.json import jsonify
 import requests
 import psycopg2
 import json
-<<<<<<< HEAD
-
-=======
 import connect_db
->>>>>>> b90c39e261fb0a0760e6d526824987ef682ffb08
 app = Flask(__name__, static_url_path='/static')
 
 @app.route("/allsubs/", methods =["POST", "GET"])
@@ -44,91 +40,11 @@ def sub():
                 return sub
 
 def random_sub():
-<<<<<<< HEAD
-    with open("settings/dblogin.json", "r") as loop:
-                    data = json.load(loop)
-
-    # Establish a connection using the dblogin.json
-    connection = connectIt()
-    cur = connection.cursor()
-    command = "SELECT * from {table} ORDER BY random()"
-    query = cur.execute(command.format(table = data["Login"]["Table"]))
-     # Fetches us all the rows so we can grab data from each
-    records = cur.fetchall()
-    for row in records:
-        sub_name = row[0]
-        last_on_sale = row[1]
-        on_sale = row[2]
-        price = row[3]
-        image = row[4]
-                    
-    # Creates a dictionary
-    data = {}
-
-    # Creates a primary catagory
-    data[sub_name.lower()] = []
-
-    # Create a default JSON structure
-    data[sub_name.lower()].append(
-    {
-        "sub_name": sub_name.lower(),
-        "last_sale": last_on_sale,
-        "status": on_sale,
-        "price": price,
-        "image": image
-    })
-        
-    return jsonify(sub_info)
-
-
-def sub_runner(subname):
-    with open("settings/dblogin.json", "r") as loop:
-                    data = json.load(loop)
-    #Establish a connection using the dblogin.json
-    connection = connectIt()   
-    cur = connection.cursor()
-    # Checks to see if the name exist in the record, then grabs a random row from that column limiting it to one.
-    try:
-        command = "SELECT * FROM {table} WHERE pubsub_name = '{name}' ORDER BY dates DESC LIMIT 1"
-        query = cur.execute(command.format(table = data["Login"]["Table"], name = subname))
-    except:
-        return "Unfortunately, we do not have deal data available on " + subname.replace("-", " ") + " sub at this time.\nOur current offers is the boar-head-jerk-turkey-and-gouda sub"
-    
-    # Fetches us all the rows so we can grab data from each
-    records = cur.fetchall()
-    for row in records:
-        last_on_sale = row[1]
-        on_sale = row[2]
-        price = row[3]
-        image = row[4]
-                    
-    # Creates a dictionary
-    data = {}
-
-    # Creates a primary catagory
-    data[subname.lower()] = []
-
-    # Create a default JSON structure
-    data[subname.lower()].append(
-    {
-        "sub_name": subname.lower(),
-        "last_sale": last_on_sale,
-        "status": on_sale,
-        "price": price,
-        "image": image
-    })
-                
-                
-    sub_info = json.dumps(data, indent = 2)
-        
-    return sub_info
-=======
     sub = connect_db.connect_random()     
     return sub
 def sub_runner(subname):
     sub = connect_db.connect(subname)         
     return sub
->>>>>>> b90c39e261fb0a0760e6d526824987ef682ffb08
                 
                
 @app.route("/", methods =["POST", "GET"])
