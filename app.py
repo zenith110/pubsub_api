5eb1df058dd1f2955e06b42436c079738df3a73a
zenith110 <thequake@live.com>
2020-09-30 16:58:04 -0400

    Finalized the allsub endpoint. Credits go to callum for portions of the code

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ app = Flask(__name__, static_url_path='/static')
 @app.route("/allsubs/", methods =["POST", "GET"])
 def all_names():
 
-    connection = connectIt()   
+    connection = connect_db.connect()   
     cur = connection.cursor()
 
-    query = "SELECT pubsub_name FROM {table} ORDER BY pubsub"
-    cur.execute(query.format(table = data["Login"]["Table"]))
+    query = "SELECT pubsub_name FROM {table} ORDER BY on_sale"
+    cur.execute(query.format(table = connect_db.get_table()))
     
     records = cur.fetchall()
 
@@ -24,11 +24,7 @@ def all_names():
 
     # Create a default JSON structure
     for sub in records:
-        records.
-        data["All_subs".lower()].append(
-        {
-            {"name": sub} #this may need to be sub[0] not sure
-        })
+        data["All_subs".lower()].append({"name": sub}) #this may need to be sub[0] not sure)
         
     return jsonify(data["All_subs".lower()])
 
@@ -44,25 +40,86 @@ def sub():
                 return sub
 
 def random_sub():
-    sub = connect_db.connect_random()     
-    return sub
+    connection = connect_db.connect()
+    cur = connection.cursor()
+    # Checks to see if the name exist in the record, then grabs a random row from that column limiting it to one.
+    command = "SELECT * FROM {table} ORDER BY random()"
+    query = cur.execute(command.format(table = connect_db.get_table()))
+    
+    # Fetches us all the rows so we can grab data from each
+    records = cur.fetchall()
+    for row in records:
+        subname = row[0]
+        last_on_sale = row[1]
+        on_sale = row[2]
+        price = row[3]
+        image = row[4]
+                    
+    # Creates a dictionary
+    data = {}
+
+    # Creates a primary catagory
+    data[subname.lower()] = []
+
+    # Create a default JSON structure
+    data[subname.lower()].append(
+    {
+        "sub_name": subname.lower(),
+        "last_sale": last_on_sale,
+        "status": on_sale,
+        "price": price,
+        "image": image
+    })
+                
+                
+    sub_info = json.dumps(data, indent = 2)
+        
+    return sub_info
+
 def sub_runner(subname):
-    sub = connect_db.connect(subname)         
-    return sub
+    connection = connect_db.connect()
+    cur = connection.cursor()
+    # Checks to see if the name exist in the record, then grabs a random row from that column limiting it to one.
+    try:
+        command = "SELECT * FROM {table} WHERE pubsub_name = '{name}' ORDER BY dates DESC LIMIT 1"
+        query = cur.execute(command.format(table = connect_db.get_table(), name = subname))
+    except:
+        return "Unfortunately, we do not have deal data available on " + subname.replace("-", " ") + " sub at this time.\nOur current offers is the boar-head-jerk-turkey-and-gouda sub"
+    
+    # Fetches us all the rows so we can grab data from each
+    records = cur.fetchall()
+    for row in records:
+        last_on_sale = row[1]
+        on_sale = row[2]
+        price = row[3]
+        image = row[4]
+                    
+    # Creates a dictionary
+    data = {}
+
+    # Creates a primary catagory
+    data[subname.lower()] = []
+
+    # Create a default JSON structure
+    data[subname.lower()].append(
+    {
+        "sub_name": subname.lower(),
+        "last_sale": last_on_sale,
+        "status": on_sale,
+        "price": price,
+        "image": image
+    })
+                
+                
+    sub_info = json.dumps(data, indent = 2)
+        
+    return sub_info
                 
                
 @app.route("/", methods =["POST", "GET"])
 def index():
         return render_template("index.html")
         
-        
-def connectIt():
-    return psycopg2.connect(user = data["Login"]["Username"],
-                                        password = data["Login"]["Password"],
-                                        host = data["Login"]["Host"],
-                                        port = data["Login"]["Port"],
-                                        database = data["Login"]["Database"]) 
-
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
--- connect_db.py
+++ connect_db.py
@@ -1,53 +1,24 @@
 import psycopg2
 import json
 """
-Queries for specified subs
+Allows us to query specific commands
 """
-def connect(subname):
+def get_table():
     with open("settings/dblogin.json", "r") as loop:
-                    data = json.load(loop)
-    #Establish a connection using the dblogin.json
-    connection = psycopg2.connect(user = data["Login"]["Username"],
+                        data = json.load(loop)
+    return data["Login"]["Table"]
+def query_call(cur, query):
+    with open("settings/dblogin.json", "r") as loop:
+                        data = json.load(loop)
+    cur.execute(query)
+def connect():
+    with open("settings/dblogin.json", "r") as loop:
+                        data = json.load(loop)
+    return psycopg2.connect(user = data["Login"]["Username"],
                                         password = data["Login"]["Password"],
                                         host = data["Login"]["Host"],
                                         port = data["Login"]["Port"],
-                                        database = data["Login"]["Database"])          
-    cur = connection.cursor()
-    # Checks to see if the name exist in the record, then grabs a random row from that column limiting it to one.
-    try:
-        command = "SELECT * FROM {table} WHERE pubsub_name = '{name}' ORDER BY dates DESC LIMIT 1"
-        query = cur.execute(command.format(table = data["Login"]["Table"], name = subname))
-    except:
-        return "Unfortunately, we do not have deal data available on " + subname.replace("-", " ") + " sub at this time.\nOur current offers is the boar-head-jerk-turkey-and-gouda sub"
-    
-    # Fetches us all the rows so we can grab data from each
-    records = cur.fetchall()
-    for row in records:
-        last_on_sale = row[1]
-        on_sale = row[2]
-        price = row[3]
-        image = row[4]
-                    
-    # Creates a dictionary
-    data = {}
-
-    # Creates a primary catagory
-    data[subname.lower()] = []
-
-    # Create a default JSON structure
-    data[subname.lower()].append(
-    {
-        "sub_name": subname.lower(),
-        "last_sale": last_on_sale,
-        "status": on_sale,
-        "price": price,
-        "image": image
-    })
-                
-                
-    sub_info = json.dumps(data, indent = 2)
-        
-    return sub_info
+                                        database = data["Login"]["Database"])    
 
 """
 Queries for random pubsubs
