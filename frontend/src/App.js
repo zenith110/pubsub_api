7eac64eb3831405e58a6101d8042e102649715d1
zenith110 <thequake@live.com>
2020-12-20 15:05:46 -0500

    Automatically scales the subcard component, no longer needed to manually input a position

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,37 @@
 import './App.css';
 import SubCard from "./Components/SubCard.js"
 import Carousel from 'react-elastic-carousel';
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect} from 'react';
 import Email from "./Components/Email.js";
 import Navbar from "./Components/Navbar.js"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  // Assigns state to count so we can add new subs
+  let[subCount, setSubCount] = useState([])
+
+  let SubCardArr = [];
+  var i;
+  // Creates an array of subcard components
+  for(i = 0; i < subCount; i++){
+    SubCardArr.push(<SubCard position={i}></SubCard>)
+  }
+  // Sends a post request for our number of subs
+  useEffect(()=>{
+    fetch('http://localhost:5000/totalcount/')
+    .then ((response) => response.json())
+    .then((data) => setSubCount(data))
+    .catch((error) => console.log(error))
+  }, [])
+
   return (
     <div className="App">
     <Navbar></Navbar>
     <p>Current Subs</p>
     <Carousel>
-    <SubCard position={0}></SubCard>
-    <SubCard position={1}></SubCard>
-    <SubCard position={2}></SubCard>
-    <SubCard position={3}></SubCard>
-    <SubCard position={4}></SubCard>
-    <SubCard position={5}></SubCard>
+    {SubCardArr}
     </Carousel>
     <Email></Email>
-    
     </div>
   );
 }
