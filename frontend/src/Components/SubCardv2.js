9a21a83db97a00902eef9e794d2ea738bbc7a88b
zenith110 <thequake@live.com>
2021-02-06 23:04:15 -0500

    Modified the docker file some more

--- frontend/src/Components/SubCardv2.js
+++ frontend/src/Components/SubCardv2.js
@@ -26,7 +26,7 @@ const SubCardv2 = ( {option}) => {
 
     // fetching pubsub data
     useEffect(()=>{
-        fetch('https://backend/onsale/')
+        fetch('http://backend/onsale/')
         .then ((response) => response.json())
         .then((data) => setSub(data))
         .catch((error) => console.log(error))
