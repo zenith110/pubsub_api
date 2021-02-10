17cd112cd69397af94d34d925adc9e75b5300d6a
zenith110 <thequake@live.com>
2021-02-09 22:19:44 -0500

    Fixed flask_limit.util issue

--- backend/v1/src/app.py
+++ backend/v1/src/app.py
@@ -10,7 +10,7 @@ from services import all_subs
 from services import random_subs
 from flasgger import Swagger, swag_from
 from flask_limiter import Limiter
-from flask_limiter import get_remote_address
+from flask_limiter.util import get_remote_address
 import docs
 app = Flask(__name__, static_url_path="/static")
 Swagger(app)
