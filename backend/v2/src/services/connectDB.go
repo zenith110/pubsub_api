55ed037482dd48b378d577955076a0a23cf74de4
zenith110 <thequake@live.com>
2021-04-16 22:27:47 -0400

    Made basic structure for db on go backend

--- backend/v2/src/services/connectDB.go
+++ backend/v2/src/services/connectDB.go
@@ -1,1 +1,21 @@
 package services
+
+type Login struct {
+	Username string `json:"Username"`
+	Password string `json:"Password"`
+	Database string `json:"Database"`
+	Host     string `json:"Host"`
+	Table    string `json:"Table"`
+	Port     string `json:"Port"`
+}
+
+func GetTable() {
+
+}
+func CloseDB() {
+
+}
+
+func ConnectToDB() {
+
+}
