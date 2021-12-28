a99bd9839867b1590e16923b23a7debfb9c193b3
zenith110 <thequake@live.com>
2021-12-27 23:07:24 -0500

    Switched over to using pgxpool for pooling, and added a total to pubsubs query

--- backend/go/src/go.mod
+++ backend/go/src/go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.9.1 // indirect
+	github.com/jackc/puddle v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v0.0.0-20180203102830-a4e142e9c047 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
--- backend/go/src/graph/queries/fetchallsubs.go
+++ backend/go/src/graph/queries/fetchallsubs.go
@@ -3,9 +3,10 @@ package queries
 import (
 	"context"
 	"fmt"
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"os"
 	"pubsub-api/graph/model"
+	"strconv"
 )
 
 // Given a table name and database url, fetch all the pubsubs from the database
@@ -15,16 +16,17 @@ func FetchAllSubs(tableName string, databaseURL string) (*model.Pubsubs, error) 
 	// Creates the background context
 	ctx := context.Background()
 	// Connect to the database
-	conn, err := pgx.Connect(ctx, databaseURL)
+	conn, err := pgxpool.Connect(ctx, databaseURL)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 	// Wait to close for later
-	defer conn.Close(ctx)
+	defer conn.Close()
 	// Query for all the subs
 	query := fmt.Sprintf("SELECT pubsub_name, dates, on_sale, price, image FROM %s WHERE pubsub_name is not NULL ORDER BY on_sale", tableName)
-	rows, connErr := conn.Query(context.Background(), query)
+	var totalSubs int = 0
+	rows, connErr := conn.Query(ctx, query)
 	if connErr != nil {
 		fmt.Fprintf(os.Stderr, "Query failed: %v\n", err)
 		os.Exit(1)
@@ -34,8 +36,9 @@ func FetchAllSubs(tableName string, databaseURL string) (*model.Pubsubs, error) 
 		var pubsub model.Pubsub
 		rows.Scan(&pubsub.Name, &pubsub.Saledates, &pubsub.Onsale, &pubsub.Price, &pubsub.Image)
 		subs = append(subs, &pubsub)
+		totalSubs += 1
 	}
 	// Creates new *model.Pubsubs struct to return to the user
-	var pubsubs = model.Pubsubs{Sub: subs}
+	var pubsubs = model.Pubsubs{Sub: subs, Total: strconv.Itoa(totalSubs)}
 	return &pubsubs, err
 }
