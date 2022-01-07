09880874b88c9e6a3ccf23a4525eb782f49c571f
zenith110 <thequake@live.com>
2022-01-06 21:15:37 -0500

    Fixed all_subs.py name

---
+++ backend/python/src/services/all_subs.py
@@ -0,0 +1,30 @@
+import connect_db
+from flask import Flask, jsonify
+
+
+def all_subs_data(db_object):
+    connection = connect_db.connect(db_object)
+    cur = connection.cursor()
+
+    query = (
+        "SELECT pubsub_name FROM {table} WHERE pubsub_name is not NULL ORDER BY on_sale"
+    )
+    cur.execute(query.format(table=connect_db.get_table(db_object)))
+
+    records = cur.fetchall()
+
+    data = {}
+
+    """
+    Creates a primary catagory
+    """
+    data["All_subs".lower()] = []
+
+    """
+    Create a default JSON structure
+    """
+    for sub in records:
+        data["All_subs".lower()].append({"name": sub[0]})
+    response = jsonify(data["All_subs".lower()])
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
