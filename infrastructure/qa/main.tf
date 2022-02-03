52776f91028b6b9400fa009fa7ba761ccc6abd6d
zenith110 <thequake@live.com>
2022-02-02 19:38:09 -0500

    Modified region

--- infrastructure/qa/main.tf
+++ infrastructure/qa/main.tf
@@ -15,7 +15,7 @@ terraform{
     }
    backend "s3" {
    key            = "state/terraform.tfstate"
-   region         = "us-east-1"
+   region         = "us-east-2"
    encrypt        = true
    kms_key_id     = "alias/terraform-bucket-key"
    dynamodb_table = "terraform-state"
