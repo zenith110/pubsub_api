df0b52336481a57e84410ab52c4f66491ed8fc4f
zenith110 <thequake@live.com>
2022-02-02 20:12:13 -0500

    Modified cloudflare record

--- infrastructure/qa/cloudflare.tf
+++ infrastructure/qa/cloudflare.tf
@@ -8,7 +8,7 @@ resource "cloudflare_record" "server"{
 
 resource "cloudflare_record" "qa-graphql"{
     zone_id = var.cloudflare_zone_id
-    name  = qa-grapqhl.pubsub-api.dev
+    name  = var.cloudflare_domain_name.pubsub-api.dev
     value = linode_instance.pubsub-qa.ip_address
     type = "A"
     proxied = true
@@ -16,7 +16,7 @@ resource "cloudflare_record" "qa-graphql"{
 
 resource "cloudflare_record" "qa-backend"{
     zone_id = var.cloudflare_zone_id
-    name  = qa-backend.pubsub-api.dev
+    name  = var.cloudflare_domain_name-backend.pubsub-api.dev
     value = linode_instance.pubsub-qa.ip_address
     type = "A"
     proxied = true
