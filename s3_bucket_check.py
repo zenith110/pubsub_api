e17e83844a686371e5eabf9b9e85007618afa4d5
zenith110 <thequake@live.com>
2022-02-04 17:41:38 -0500

    Fixed bucket code

--- s3_bucket_check.py
+++ s3_bucket_check.py
@@ -6,18 +6,20 @@ Checks if the bucket for the specific pull request exist. This will occur on the
 If it does, returns a success and triggers a job that will init the terraform creation of the terraform state on the bucket
 If it does not, create the bucket, then run the init
 """
-def bucket_exist():
-    s3 = boto3.resource(
-        service_name="s3",
-        region_name="us-east-2",
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    )
-
-    try:
-        bucket = s3.meta.client.head_bucket(Bucket=os.getenv("BUCKET_NAME"))
-        print(bucket)
-    except ClientError:
-        return ClientError
-if __name__ == "__main__":
-    bucket_exist()
+s3 = boto3.resource(
+    service_name="s3",
+    region_name="us-east-2",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
+bucket = s3.Bucket(os.getenv("BUCKET_NAME"))
+exists = True
+try:
+    bucket = s3.meta.client.head_bucket(Bucket=os.getenv("BUCKET_NAME"))
+    print(bucket)
+except ClientError as e:
+# If a client error is thrown, then check that it was a 404 error.
+# If it was a 404 error, then the bucket does not exist.
+    error_code = e.response['Error']['Code']
+    if error_code == '404':
+        exists = False
