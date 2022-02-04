50240d56c8e030f940e656d7249cd25cba55f4ad
zenith110 <thequake@live.com>
2022-02-04 13:13:52 -0500

    Paused creation of bucket job and removed default record for new site

--- infrastructure/qa/cloudflare.tf
+++ infrastructure/qa/cloudflare.tf
@@ -1,11 +1,3 @@
-resource "cloudflare_record" "server"{
-    zone_id = var.cloudflare_zone_id
-    name  = var.cloudflare_domain_name
-    value = linode_instance.pubsub-qa.ip_address
-    type = "A"
-    proxied = true
-}
-
 resource "cloudflare_record" "qa-graphql"{
     zone_id = var.cloudflare_zone_id
     name  = var.qa-graphql
