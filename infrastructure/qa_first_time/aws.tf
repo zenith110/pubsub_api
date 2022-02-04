fa98bd3b1ee2a37960fd0e268654a3a4cea1a485
zenith110 <thequake@live.com>
2022-02-04 18:23:33 -0500

    Updating keys

--- infrastructure/qa_first_time/aws.tf
+++ infrastructure/qa_first_time/aws.tf
@@ -1,5 +1,4 @@
-resource "aws_kms_key" "terraform-bucket-key" {
- key_id = var.qa-backend
+resource "aws_kms_key" "qa-terraform-bucket-key" {
  description             = "This key is used to encrypt bucket objects"
  deletion_window_in_days = 10
  enable_key_rotation     = true
