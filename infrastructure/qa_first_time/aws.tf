f4bf3afca6234d82a0f1e3f6cdaa04931f806343
zenith110 <thequake@live.com>
2022-02-04 18:28:15 -0500

    Added kms alias for keys

--- infrastructure/qa_first_time/aws.tf
+++ infrastructure/qa_first_time/aws.tf
@@ -1,11 +1,11 @@
-resource "aws_kms_key" "qa-terraform-bucket-key" {
+resource "aws_kms_key" "terraform-bucket-key" {
  description             = "This key is used to encrypt bucket objects"
  deletion_window_in_days = 10
  enable_key_rotation     = true
 }
 
 resource "aws_kms_alias" "key-alias" {
- name          = "alias/terraform-bucket-key"
+ name          = var.kms_alias
  target_key_id = aws_kms_key.terraform-bucket-key.key_id
 }
 
