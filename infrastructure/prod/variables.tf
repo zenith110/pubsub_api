3ff0547df0390e415fb1effd14f9c3cac5bd2790
zenith110 <thequake@live.com>
2022-02-06 12:31:11 -0500

    Added prod variables and vercel

---
+++ infrastructure/prod/variables.tf
@@ -0,0 +1,42 @@
+variable "token" {
+      description = "Your Linode API Personal Access Token. (required)"
+    }
+
+    variable "k8s_version" {
+      description = "The Kubernetes version to use for this cluster. (required)"
+      default = "1.17"
+    }
+
+    variable "label" {
+      description = "The unique label to assign to this cluster. (required)"
+      default = "default-lke-cluster"
+    }
+
+    variable "region" {
+      description = "The region where your cluster will be located. (required)"
+      default = "us-east"
+    }
+
+    variable "tags" {
+      description = "Tags to apply to your cluster for organizational purposes. (optional)"
+      type = list(string)
+      default = ["testing"]
+    }
+
+    variable "pools" {
+      description = "The Node Pool specifications for the Kubernetes cluster. (required)"
+      type = list(object({
+        type = string
+        count = number
+      }))
+      default = [
+        {
+          type = "g6-nanode-1"
+          count = 3
+        },
+        {
+          type = "g6-nanode-1"
+          count = 3
+        }
+      ]
+    }
---
+++ infrastructure/qa_first_time/vercel.tf
@@ -0,0 +1,24 @@
+data "vercel_project_directory" "example" {
+  path = "./frontend"
+}
+
+data "vercel_project" "example" {
+  name = var.github_branch
+}
+
+resource "vercel_deployment" "example" {
+  project_id = data.vercel_project.example.id
+  files      = data.vercel_project_directory.example.files
+  production = false
+
+  project_settings = {
+    output_directory = "/build"
+    build_command    = "npm run build"
+    framework        = "create-react-app"
+    root_directory   = "./frontend"
+  }
+
+  environment = {
+    REACT_APP_PUBSUB_API_URL = cloudflare_record.qa-backend.name
+  }
+}
